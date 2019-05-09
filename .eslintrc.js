e0f5799aa34352a98c4ee196e5a172d375c4c092
Dennis Wethmar <Dennis.Wethmar@macaw.nl>
2019-05-09 13:08:21 +0200

    Fixed some linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
+        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -15,6 +16,11 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        'quote-props': [2, 'consistent-as-needed'],
+        'quotes': [2, 'single', {
+            avoidEscape: true
+        }],
+        'react/prop-types': false
     },
     settings: {
         react: {
